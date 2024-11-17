9bab60e8c53ea67cf21089042c04863709a2c70b
Yeil-sr <Blackfootout@gmail.com>
2024-11-17 03:36:36 -0200

    modified:   frontend/src/Pages/Shop.jsx

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import NewCollections from '../Components/NewCollections/NewCollections'
-// import Popular from '../Components/Popular/Popular';
+import Popular from '../Components/Popular/Popular';
 import Offers from '../Components/Offers/Offers';
+import Popular from '../Components/Popular/Popular';
 
 
 const Shop = ()=>{
     return (
         <div>
             <Hero/>
-            <NewCollections/>
+            <Popular/>
             <Offers/>
             <NewCollections/>
         </div>
